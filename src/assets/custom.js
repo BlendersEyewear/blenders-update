561b34c5d497cc8ae18f607e073afc6606ad70ad
Adam Rasheed <adam@blenderseyewear.com>
2018-04-16 17:46:21 -0700

    added product tabs and mobile styles

--- src/assets/custom.js
+++ src/assets/custom.js
@@ -133,6 +133,61 @@ const resizeHeadings = function(){
 }
 
 
+//------------------------------------------------
+//--------- Custom Product Page ------------------
+//------------------------------------------------
+const customProductPage = function(){
+  $('.product-tabs__titles').each(function(){
+
+    var $active, $content, $links = $(this).find('a');
+
+    // If the location.hash matches one of the links, use that as the active tab.
+    // If no match is found, use the first link as the initial active tab.
+    $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+    $active.addClass('active');
+
+    $content = $($active[0].hash);
+
+    // Hide the remaining content
+    $links.not($active).each(function () {
+      $(this.hash).hide();
+    });
+
+    // Bind the click event handler
+    $(this).on('click', 'a', function(e){
+      // Make the old tab inactive.
+      $active.removeClass('active');
+      $content.hide();
+
+      // Update the variables with the new link and content
+      $active = $(this);
+      $content = $(this.hash);
+
+      // Make the tab active.
+      $active.addClass('active');
+      $content.show();
+
+      // Prevent the anchor's default click action
+      e.preventDefault();
+    });
+  });
+
+  // Mobile Size Guide
+
+  // Hide mobile fit content
+  $('#mobileFitContent').hide();
+  // Click function
+  $('#mobileFitToggle').click(function(){
+    console.log('wohoo');
+    // rotate arrow icon
+    $('.mobile-product-fit-icon').attr('data-fa-transform', $('.mobile-product-fit-icon').attr('data-fa-transform') == 'rotate--90' ? '' : 'rotate--90');
+    // toggle slide fit guide content
+    $('#mobileFitContent').slideToggle(250);
+  });
+}
+
+
+
 
 //  Doc Ready
 document.addEventListener("DOMContentLoaded", function() {
@@ -146,5 +201,8 @@ document.addEventListener("DOMContentLoaded", function() {
   // disabled this for now
   // screenSize <= tabletSize ? (productTabs(), resizeHeadings()) : productTabsDesktop();
   screenSize <= tabletSize ? (productTabs(), resizeHeadings()) : '';
+
+  // Run custom product page scripts if on custom product page.
+  body.classList.contains('product-template-custom') ? customProductPage() : '';
 
 });
